620e5a8fffad04523729be63a3d61afe8b91f1f8
Nergiz Nadirli <nergiznadirli@Nergiz-MacBook-Air.local>
2024-06-04 19:03:50 +0400

    update

--- UniversityApi/UniversityApi/Controllers/GroupsController.cs
+++ UniversityApi/UniversityApi/Controllers/GroupsController.cs
@@ -23,101 +23,63 @@ namespace UniversityApi.Controllers
             _dbContext = dbContext;
         }
 
-        public static List<Group> groups = new List<Group>();
-        public GroupsController()
-        {
-            groups = new List<Group>
-            {
-                new Group
-                {
-                    No = "PB301",
-                    Id=1,
-                    Limit = 10,
-                    CreatedAt = DateTime.Now.AddYears(-1),
-                    ModifiedAt = DateTime.Now.AddMonths(-2)
-                },
-                new Group
-                {
-                    No = "PB302",
-                    Id=2,
-                    Limit = 14,
-                    CreatedAt = DateTime.Now.AddYears(-2),
-                    ModifiedAt = DateTime.Now.AddMonths(-4)
-                }
-            };
-        }
         [HttpGet("")]
-        public async Task<ActionResult<IEnumerable<GroupGetDto>>> GetAll()
+        public ActionResult<List<GroupGetDto>> GetAll()
         {
-            if (_dbContext.Groups==null)
-            {
-                return NotFound();
-            }
-            var groups = await _dbContext.Groups.ToListAsync();
-            var dtos = groups.Select(x => new GroupGetDto
+            List<GroupGetDto> dtos = _dbContext.Groups.Select(x => new GroupGetDto
             {
                 Id = x.Id,
                 No = x.No,
                 Limit = x.Limit
             }).ToList();
-            return StatusCode(200,dtos);
+
+            return StatusCode(200, dtos);
         }
 
-
         [HttpGet("{id}")]
-        public async Task<ActionResult<GroupGetDto>> GetById(int id)
+        public ActionResult<GroupGetDto> GetById(int id)
         {
-            if (_dbContext.Groups == null)
+            var data = _dbContext.Groups.FirstOrDefault(x => x.Id == id);
+
+            if (data == null)
             {
-                return NotFound();
-            }
-            var group = await _dbContext.Groups.FindAsync(id);
-            if (group == null)
-            {
-                return NotFound();
+                return StatusCode(404);
             }
 
-            var dto = new GroupGetDto
+            GroupGetDto dto = new GroupGetDto
             {
-                Id = group.Id,
-                No = group.No,
-                Limit = group.Limit
+                Id = data.Id,
+                No = data.No,
+                Limit = data.Limit
             };
-            return StatusCode(201, dto);
+            return StatusCode(200, dto);
         }
 
 
         [HttpPost("")]
-        public async Task<ActionResult<GroupGetDto>> Create([FromBody] GroupCreateDto createDto)
+        public ActionResult Create(GroupCreateDto createDto)
         {
             var newGroup = new Group
             {
                 No = createDto.No,
                 Limit = createDto.Limit,
                 CreatedAt = DateTime.Now,
-                ModifiedAt = DateTime.Now
+             
             };
 
             _dbContext.Groups.Add(newGroup);
-            await _dbContext.SaveChangesAsync();
+            _dbContext.SaveChangesAsync();
 
-            var dto = new GroupGetDto
-            {
-                Id = newGroup.Id,
-                No = newGroup.No,
-                Limit = newGroup.Limit
-            };
-
-            return CreatedAtAction(nameof(GetById), new { id = newGroup.Id }, dto);
+            return StatusCode(200);
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<GroupGetDto>> Update(int id, [FromBody] GroupUpdateDto updateDto)
+        public ActionResult Update(int id, GroupUpdateDto updateDto)
         {
-            var group = await _dbContext.Groups.FindAsync(id);
+            var group = _dbContext.Groups.Find(id);
             if (group == null)
             {
-                return NotFound();
+                return StatusCode(404);
             }
 
             group.No = updateDto.No;
@@ -125,30 +87,24 @@ namespace UniversityApi.Controllers
             group.ModifiedAt = DateTime.Now;
 
             _dbContext.Groups.Update(group);
-            await _dbContext.SaveChangesAsync();
+            _dbContext.SaveChangesAsync();
 
-            var dto = new GroupGetDto
-            {
-                Id = group.Id,
-                No = group.No,
-                Limit = group.Limit
-            };
 
-            return Ok(dto);
+            return StatusCode(200);
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
+        public ActionResult Delete(int id)
         {
-            var group = await _dbContext.Groups.FindAsync(id);
+            var group = _dbContext.Groups.Find(id);
             if (group == null)
             {
-                return NotFound();
+                return StatusCode(404);
             }
 
             _dbContext.Groups.Remove(group);
-            await _dbContext.SaveChangesAsync();
-            return NoContent();
+          _dbContext.SaveChangesAsync();
+             return StatusCode(200);
         }
 
         // GET: api/values
